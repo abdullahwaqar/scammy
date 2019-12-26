bf3dbc1d8820bcc344bbb8fd008ec0936fefe7ee
awr417h <abdullahwaqar8@gmail.com>
2019-12-26 22:54:02 +0500

    changed to deployed api url

--- client/src/Network.js
+++ client/src/Network.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = `http://localhost:4000/api/v1.0`;
+const BASE_URL = `https://scammy-api.herokuapp.com/api/v1.0`;
 
 async function ping() {
     return await axios.get(`${BASE_URL}/ping`);
